3f00fb0e73430e461828c080842ad93964bd3566
Sevag Avedissian <sevag.avedissian@gmail.com>
2023-02-22 23:31:24 -0800

    add location

--- components/RestaurantList.jsx
+++ components/RestaurantList.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import {
-  Button,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  ScrollView,
-  FlatList,
+    Button,
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+    ScrollView,
+    FlatList,
 } from "react-native";
 import config from "../services/api.json";
 import Constants from "expo-constants";
@@ -14,80 +14,116 @@ import GoalItem from "./GoalItem";
 import RestaurantItem from "./RestaurantItem";
 import axios from "axios";
 
+import Device from "expo-device";
+import * as Location from "expo-location";
+
 const { manifest } = Constants;
 
 const api = `http://${manifest.debuggerHost
-  .split(":")
-  .shift()}:8000/api/useritems/?location=999,88`;
+    .split(":")
+    .shift()}:8000/api/useritems/?location=`;
 
-const RestaurantList = ({navigation}) => {
-  const [restaurants, setRestaurants] = useState([]);
-  const [localRestaurants, setLocalRestaurants] = useState([]);
+const RestaurantList = ({ navigation }) => {
+    const [restaurants, setRestaurants] = useState([]);
+    const [localRestaurants, setLocalRestaurants] = useState([]);
 
-  useEffect(() => {
-    getRestaurants();
-  }, []);
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
 
-  let getLocalRestaurants = (data) => {
-    const uniqueRestaurants = [...new Map(data.map(v => [v.restaurant_id, v])).values()]
-    setLocalRestaurants(uniqueRestaurants)
-  };
+    useEffect(() => {
+        getLocation();
+        getRestaurants();
+    }, []);
 
-  const getRestaurants = async () => {
-    try {
-      const response = await axios.get(api)
-      setRestaurants(response.data)
-      getLocalRestaurants(response.data)
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    let getLocalRestaurants = (data) => {
+        const uniqueRestaurants = [
+            ...new Map(data.map((v) => [v.restaurant_id, v])).values(),
+        ];
+        setLocalRestaurants(uniqueRestaurants);
+    };
 
+    const getRestaurants = async () => {
+        try {
+            const response = await axios.get(
+                api +
+                    location.LocationObjectCoords.latitude +
+                    "," +
+                    location.LocationObjectCoords.longitude
+            );
+            setRestaurants(response.data);
+            getLocalRestaurants(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-  let onHandlePageChange = (id) => {
-    const chosenRestaurantItems = restaurants.filter(restaurant => {
-      return restaurant.restaurant_id === id;
-    })
-    navigation.navigate('Restaurant', {restaurant_id: id, restaurant_items: chosenRestaurantItems})
-  }
+    const getLocation = async () => {
+        if (Platform.OS === "android" && !Device.isDevice) {
+            setErrorMsg(
+                "Oops, this will not work on Snack in an Android Emulator. Try it on your device!"
+            );
+            return;
+        }
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== "granted") {
+            setErrorMsg("Permission to access location was denied");
+            return;
+        }
 
-  let junk = () => {
-    return 0
-  }
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+    };
 
-  return (
-    <View>
-      <View style={styles.textBox}>
-        <Text style={styles.text}>{"Restaurants"}</Text>
-        <FlatList 
-            data={localRestaurants} 
-            renderItem={itemData => {
-              return <RestaurantItem 
-                        text={itemData.item.restaurant_name} 
-                        id={itemData.item.restaurant_id}
-                        onPageChange={onHandlePageChange}/>
-            }}
-            keyExtractor={(item, index) => {
-              return item.id
-            } }  
-          />
-      </View>
-    </View>
-  );
+    let onHandlePageChange = (id) => {
+        const chosenRestaurantItems = restaurants.filter((restaurant) => {
+            return restaurant.restaurant_id === id;
+        });
+        navigation.navigate("Restaurant", {
+            restaurant_id: id,
+            restaurant_items: chosenRestaurantItems,
+        });
+    };
+
+    let junk = () => {
+        return 0;
+    };
+
+    return (
+        <View>
+            <View style={styles.textBox}>
+                <Text style={styles.text}>{"Restaurants"}</Text>
+                <FlatList
+                    data={localRestaurants}
+                    renderItem={(itemData) => {
+                        return (
+                            <RestaurantItem
+                                text={itemData.item.restaurant_name}
+                                id={itemData.item.restaurant_id}
+                                onPageChange={onHandlePageChange}
+                            />
+                        );
+                    }}
+                    keyExtractor={(item, index) => {
+                        return item.id;
+                    }}
+                />
+            </View>
+        </View>
+    );
 };
 
 export default RestaurantList;
 
 const styles = StyleSheet.create({
-  textBox: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: "#5e0acc",
-    alignItems: "center",
-  },
-  text: {
-    color: "white",
-    //fontSize: "25px",
-  },
+    textBox: {
+        margin: 8,
+        padding: 8,
+        borderRadius: 6,
+        backgroundColor: "#5e0acc",
+        alignItems: "center",
+    },
+    text: {
+        color: "white",
+        //fontSize: "25px",
+    },
 });
