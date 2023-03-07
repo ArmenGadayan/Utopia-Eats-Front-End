b983890274e15cfd9836755c4b5cff0f7dc32f9c
Sevag Avedissian <sevag.avedissian@gmail.com>
2023-03-06 19:34:38 -0800

    hiding navbar from login

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,104 +1,104 @@
 import React, { useState, useContext } from "react";
 import AuthContext from "../context/AuthContext";
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  TextInput,
-  TouchableOpacity,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+    TextInput,
+    TouchableOpacity,
 } from "react-native";
 import * as RootNavigation from "../utils/RootNavigation";
 
 const NavBar = ({ navigation }) => {
-  const [state, setState] = useState({
-    username: "",
-    password: "",
-  });
+    const [state, setState] = useState({
+        username: "",
+        password: "",
+    });
 
-  const authContext = useContext(AuthContext);
+    const authContext = useContext(AuthContext);
 
-  if (authContext === null) {
-    return (<></>)
-  }
+    if (authContext.user === null) {
+        return <></>;
+    }
 
-  const onPressLogin = () => {
-    authContext.loginUser(state, navigation);
-  };
+    const onPressLogin = () => {
+        authContext.loginUser(state, navigation);
+    };
 
-  const navigateToProfile = () => {
-    RootNavigation.navigate("Profile");
-  };
+    const navigateToProfile = () => {
+        RootNavigation.navigate("Profile");
+    };
 
-  const navigateToRestaurants = () => {
-    RootNavigation.navigate("Restaurant List");
-  };
+    const navigateToRestaurants = () => {
+        RootNavigation.navigate("Restaurant List");
+    };
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.tabName}>
-        <TouchableOpacity onPress={navigateToProfile}>
-          <Text style={styles.tab}>Profile</Text>
-        </TouchableOpacity>
-      </View>
+    return (
+        <View style={styles.container}>
+            <View style={styles.tabName}>
+                <TouchableOpacity onPress={navigateToProfile}>
+                    <Text style={styles.tab}>Profile</Text>
+                </TouchableOpacity>
+            </View>
 
-      <View>
-        <TouchableOpacity onPress={navigateToRestaurants}>
-          <Text style={styles.tab}>Restaurants</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+            <View>
+                <TouchableOpacity onPress={navigateToRestaurants}>
+                    <Text style={styles.tab}>Restaurants</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 };
 
 export default NavBar;
 
 const styles = StyleSheet.create({
-  container: {
-    height: 100,
-    width: "100%",
-    backgroundColor: "black",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "row",
-  },
-  tabName: {
-    padding: 17
-  },
-  title: {
-    fontWeight: "bold",
-    fontSize: 50,
-    color: "black",
-    marginBottom: 40,
-  },
-  inputView: {
-    width: "80%",
-    backgroundColor: "#3AB4BA",
-    borderRadius: 25,
-    height: 50,
-    marginBottom: 20,
-    justifyContent: "center",
-    padding: 20,
-  },
-  inputText: {
-    height: 50,
-    color: "white",
-  },
-  tab: {
-    color: "white",
-    fontSize: 15,
-  },
-  loginBtn: {
-    width: "80%",
-    backgroundColor: "green",
-    borderRadius: 25,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
-    marginBottom: 10,
-  },
+    container: {
+        height: 100,
+        width: "100%",
+        backgroundColor: "black",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "row",
+    },
+    tabName: {
+        padding: 17,
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 50,
+        color: "black",
+        marginBottom: 40,
+    },
+    inputView: {
+        width: "80%",
+        backgroundColor: "#3AB4BA",
+        borderRadius: 25,
+        height: 50,
+        marginBottom: 20,
+        justifyContent: "center",
+        padding: 20,
+    },
+    inputText: {
+        height: 50,
+        color: "white",
+    },
+    tab: {
+        color: "white",
+        fontSize: 15,
+    },
+    loginBtn: {
+        width: "80%",
+        backgroundColor: "green",
+        borderRadius: 25,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 40,
+        marginBottom: 10,
+    },
 });
--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -14,143 +14,152 @@ const AuthContext = createContext();
 
 export default AuthContext;
 
-export const AuthProvider = ({ children}) => {
-  //   let [user, setUser] = useState(() =>
-  //     localStorage.getItem("authTokens")
-  //       ? jwt_decode(localStorage.getItem("authTokens"))
-  //       : null
-  //   );
+export const AuthProvider = ({ children }) => {
+    //   let [user, setUser] = useState(() =>
+    //     localStorage.getItem("authTokens")
+    //       ? jwt_decode(localStorage.getItem("authTokens"))
+    //       : null
+    //   );
 
-  const getUserData = async () => {
-    try {
-      const value = await AsyncStorage.getItem("authTokens");
-      if (value !== null) {
-        return jwt_decode(value);
-      }
-      return null;
-    } catch (e) {
-      return null;
-    }
-  };
-  let [user, setUser] = useState(getUserData());
+    const getUserData = () => {
+        try {
+            const value = AsyncStorage.getItem("authTokens");
+            if (value !== null) {
+                return jwt_decode(value);
+            }
+            return null;
+        } catch (e) {
+            return null;
+        }
+    };
+    let [user, setUser] = useState(getUserData());
 
+    //   let [authTokens, setAuthTokens] = useState(() =>
+    //     localStorage.getItem("authTokens")
+    //       ? JSON.parse(localStorage.getItem("authTokens"))
+    //       : null
+    //   );
 
-  //   let [authTokens, setAuthTokens] = useState(() =>
-  //     localStorage.getItem("authTokens")
-  //       ? JSON.parse(localStorage.getItem("authTokens"))
-  //       : null
-  //   );
+    const getAuthData = () => {
+        try {
+            const value = AsyncStorage.getItem("authTokens");
+            if (value !== null) {
+                return JSON.parse(value);
+            }
+            return null;
+        } catch (e) {
+            return null;
+        }
+    };
+    let [authTokens, setAuthTokens] = useState(getAuthData());
 
-  const getAuthData = async () => {
-    try {
-      const value = await AsyncStorage.getItem("authTokens");
-      if (value !== null) {
-        return JSON.parse(value);
-      }
-      return null;
-    } catch (e) {
-      return null;
-    }
-  };
-  let [authTokens, setAuthTokens] = useState(getAuthData());
+    //   let [userProfile, setUserProfile] = useState(() =>
+    //     localStorage.getItem("userProfile")
+    //       ? JSON.parse(localStorage.getItem("userProfile"))
+    //       : null
+    //   );
 
-  //   let [userProfile, setUserProfile] = useState(() =>
-  //     localStorage.getItem("userProfile")
-  //       ? JSON.parse(localStorage.getItem("userProfile"))
-  //       : null
-  //   );
+    const getUserProf = () => {
+        try {
+            const value = AsyncStorage.getItem("userProfile");
+            if (value !== null) {
+                return JSON.parse(value);
+            }
+            return null;
+        } catch (e) {
+            return null;
+        }
+    };
+    let [userProfile, setUserProfile] = useState(getUserProf());
 
-  const getUserProf = async () => {
-    try {
-      const value = await AsyncStorage.getItem("userProfile");
-      if (value !== null) {
-        return JSON.parse(value);
-      }
-      return null;
-    } catch (e) {
-      return null;
-    }
-  };
-  let [userProfile, setUserProfile] = useState(getUserProf());
+    let loginUser = async (object, navigation) => {
+        //if (!object) e.preventDefault();
 
-  let loginUser = async (object, navigation) => {
-    //if (!object) e.preventDefault();
+        const obj = { password: object.password, username: object.username };
 
-    const obj = {password: object.password, username: object.username}
+        //   ? object
+        //   : {
+        //       username: e.target.username.value,
+        //       password: e.target.password.value,
+        //     };
 
-    //   ? object
-    //   : {
-    //       username: e.target.username.value,
-    //       password: e.target.password.value,
-    //     };
+        try {
+            const response = await axios.post(api + "/auth/jwt/create/", obj);
 
-    try {
-      const response = await axios.post(api + "/auth/jwt/create/", obj);
+            if (response.status === 200) {
+                setAuthTokens(response.data);
 
-      if (response.status === 200) {
-        setAuthTokens(response.data);
+                setUser(jwt_decode(response.data.access));
+                AsyncStorage.setItem(
+                    "authTokens",
+                    JSON.stringify(response.data)
+                );
 
-        setUser(jwt_decode(response.data.access));
-        AsyncStorage.setItem("authTokens", JSON.stringify(response.data));
+                getUserProfile(response.data.access);
+                navigation.navigate("Restaurant List");
+            } else {
+                alert("Invalid input placed");
+            }
+        } catch (err) {
+            console.log(`Error: ${err.message}`);
+            console.error("Invalid username or password.");
+        }
+    };
 
-        getUserProfile(response.data.access);
-        navigation.navigate("Restaurant List")
-      } else {
-        alert("Invalid input placed");
-      }
-    } catch (err) {
-      console.log(`Error: ${err.message}`);
-      console.error("Invalid username or password.");
-    }
-  };
+    let logoutUser = () => {
+        setAuthTokens(null);
+        setUser(null);
+        setUserProfile(null);
+        AsyncStorage.removeItem("authTokens");
+        AsyncStorage.removeItem("userProfile");
+    };
 
-  let logoutUser = () => {
-    setAuthTokens(null);
-    setUser(null);
-    AsyncStorage.removeItem("authTokens");
-    AsyncStorage.removeItem("userProfile");
-  };
+    let registerUser = async (objectVals, navigation) => {
+        let obj = objectVals;
+        try {
+            const response = await axios.post(api + "/auth/users/", obj);
 
-  let registerUser = async (objectVals, navigation) => {
-    let obj = objectVals
-    try {
-      const response = await axios.post(api + "/auth/users/", obj);
+            if (response.status >= 200 && response.status < 300) {
+                const object = {
+                    username: obj.username,
+                    password: obj.password,
+                };
+                loginUser(object, navigation);
+            } else {
+                alert("Invalid input placed");
+            }
+        } catch (err) {
+            console.log(`Error: ${err.message}`);
+            console.error("Username or email already exists.");
+        }
+    };
 
-      if (response.status >= 200 && response.status < 300) {
-        const object = { username: obj.username, password: obj.password };
-        loginUser(object, navigation);
-      } else {
-        alert("Invalid input placed");
-      }
-    } catch (err) {
-      console.log(`Error: ${err.message}`);
-      console.error("Username or email already exists.");
-    }
-  };
+    let getUserProfile = async (token) => {
+        try {
+            const response = await axios.get(api + "/auth/users/me/", {
+                headers: { Authorization: "JWT " + token },
+            });
+            setUserProfile(response.data);
+            AsyncStorage.setItem("userProfile", JSON.stringify(response.data));
+        } catch (err) {
+            console.log(`Error: ${err.message}`);
+        }
+    };
 
-  let getUserProfile = async (token) => {
-    try {
-      const response = await axios.get(api + "/auth/users/me/", {
-        headers: { Authorization: "JWT " + token },
-      });
-      setUserProfile(response.data);
-      AsyncStorage.setItem("userProfile", JSON.stringify(response.data));
-    } catch (err) {
-      console.log(`Error: ${err.message}`);
-    }
-  };
+    let contextData = {
+        token: authTokens,
+        user: user,
+        userProfile: userProfile,
+        loginUser: loginUser,
+        logoutUser: logoutUser,
+        registerUser: registerUser,
+        getUserProfile: getUserProfile,
+        getAuthData: getAuthData,
+    };
 
-  let contextData = {
-    token: authTokens,
-    user: user,
-    userProfile: userProfile,
-    loginUser: loginUser,
-    logoutUser: logoutUser,
-    registerUser: registerUser,
-    getUserProfile: getUserProfile,
-  };
-
-  return (
-    <AuthContext.Provider value={contextData}>{children}</AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider value={contextData}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
